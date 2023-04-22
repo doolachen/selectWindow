fbc177ac363e7c3958da722f96126c0ee4e47a80
doolachen <919550793@qq.com>
2023-04-23 00:55:34 +0800

    fix

--- selectWindow.py
+++ selectWindow.py
@@ -3,14 +3,6 @@ import numpy as np
 import pandas as pd
 import copy
 
-"""A general prefetch generator.
-Ref:
-https://stackoverflow.com/questions/7323664/python-generator-pre-fetch
-Args:
-    generator: Python generator.
-    num_prefetch_queue (int): Number of prefetch queue.
-"""
-
 
 def select(time: list, data: list, bandwidth: float, limit: float, mode: str):
     """
@@ -20,38 +12,46 @@ def select(time: list, data: list, bandwidth: float, limit: float, mode: str):
         :param data: A datatable list.
         :param bandwidth: The throughput.
         :param limit: Time limitation.
+        :param mode:
     :returns
         list[tuple(str: windows_size,float: best_psnr)]
     """
-    df = {'3': pd.read_csv('./psnr3.csv').to_numpy(), '4': pd.read_csv('./psnr4.csv').to_numpy(),
-          '5': pd.read_csv('./psnr5.csv').to_numpy(), '6': pd.read_csv('./psnr6.csv').to_numpy(),
-          '7': pd.read_csv('./psnr7.csv').to_numpy()}
+    if mode == "all":
+        df = {'3': pd.read_csv('./psnr3.csv').to_numpy(), '4': pd.read_csv('./psnr4.csv').to_numpy(),
+              '5': pd.read_csv('./psnr5.csv').to_numpy(), '6': pd.read_csv('./psnr6.csv').to_numpy(),
+              '7': pd.read_csv('./psnr7.csv').to_numpy()}
+    else:
+        # TODO
+        df = {'3': pd.read_csv('./psnr3.csv').to_numpy(), '4': pd.read_csv('./psnr4.csv').to_numpy(),
+              '5': pd.read_csv('./psnr5.csv').to_numpy(), '6': pd.read_csv('./psnr6.csv').to_numpy(),
+              '7': pd.read_csv('./psnr7.csv').to_numpy()}
 
     total = np.sum([time, [_ / bandwidth for _ in data]], axis=0)
     windows = []
     for i, time in enumerate(total):
         if time <= limit:
-            windows.append(i+3)
+            windows.append(i + 3)
     n_frames = []
     for i in windows:
-        n_frames.append((str(i), math.floor(total[i-3]/30)))
+        n_frames.append((str(i), math.floor(total[i - 3] / 30)))
     out = {}
-    lenth = None
+    length = None
     for temp in n_frames:
         avg = []
         for line in df[temp[0]]:
-            avg.append(np.mean(line[2:2+temp[1]]))
+            avg.append(np.mean(line[2:2 + temp[1]]))
         out[temp[0]] = avg
-        lenth = len(avg)
+        length = len(avg)
     final_out = []
-    for i in range(lenth):
+    for i in range(length):
         compare = []
         for item in out:
-            compare.append((item,out[item][i]))
-        res = sorted(compare,reverse=True,key = lambda x: x[1])
+            compare.append((item, out[item][i]))
+        res = sorted(compare, reverse=True, key=lambda x: x[1])
         final_out.append(res[0])
 
     return final_out
+
 
 # 数据预处理
 # df = {'3': pd.read_csv('./psnr3.csv').to_numpy(), '4': [], '5': [], '6': [],
@@ -103,4 +103,5 @@ d = [20, 20, 30, 40, 50]
 b = 0.5
 L = 100
 
-select(t, d, b, L,mode='all')
+a = select(t, d, b, L, mode='all')
+print(a)
